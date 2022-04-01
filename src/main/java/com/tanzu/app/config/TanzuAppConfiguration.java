c3f4b7d88593a909f777b8966c55ad3974f86aca
Fabien MARTIN <fmartin@vmware.com>
2022-04-01 10:47:08 +0200

    Spring Native

--- src/main/java/com/tanzu/app/config/TanzuAppConfiguration.java
+++ src/main/java/com/tanzu/app/config/TanzuAppConfiguration.java
@@ -5,10 +5,6 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
 public class TanzuAppConfiguration {
 
@@ -16,15 +12,6 @@ public class TanzuAppConfiguration {
   @Bean
   public HttpTraceRepository htttpTraceRepository() {
     return new InMemoryHttpTraceRepository();
-  }
-
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-        .components(new Components())
-        .info(new Info()
-            .title("Tanzu App - OpenAPI 3.0")
-            .description("This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3."));
   }
 
 }
